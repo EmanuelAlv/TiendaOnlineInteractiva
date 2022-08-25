571f2bdb492c9d354c57eef5859d75868ae3f680
Emanuel Alvarez <alvarezema9@gmail.com>
2022-08-25 15:29:26 -0600

    Se Anadio el menú de carrito de compras

--- main.js
+++ main.js
@@ -1,7 +1,36 @@
 const navbarEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
+const burguerMenuIcon = document.querySelector('.burguer-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+const cartIcon = document.querySelector('.navbar-shopping-cart');
+const cartMenu = document.querySelector('.product-detail');
+
 navbarEmail.addEventListener('click', toggleDesktopMenu);
+burguerMenuIcon.addEventListener('click', toggleMobileMenu);
+cartIcon.addEventListener('click', toggleCartMenu);
 
 function toggleDesktopMenu () {
+    const isCartMenuClosed = cartMenu.classList.contains('inactive');
+    if (!isCartMenuClosed){
+        cartMenu.classList.add('inactive');
+    }
     desktopMenu.classList.toggle('inactive')
+}
+function toggleMobileMenu () {
+    const isCartMenuClosed = cartMenu.classList.contains('inactive');
+    if (!isCartMenuClosed){
+        cartMenu.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle('slide-right')
+}
+function toggleCartMenu () {
+    const isMobileMenuOpen = mobileMenu.classList.contains('slide-right');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+    if (isMobileMenuOpen){
+        mobileMenu.classList.remove('slide-right');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    cartMenu.classList.toggle('inactive')
 }
